f318f6eaf98bc8ab6566df13a809856392422d1b
Aleksey Rakov <Aleksey.Rakov@wisetechglobal.com>
2024-05-28 15:30:56 +0200

    working on vscode extension

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { ExtensionContext } from 'vscode';
+
+/**
+ * This method is called when the extension is activated.
+ * @param context The extension context
+ */
+export function activate(context: ExtensionContext): void {
+}
+
+export function deactivate(): void {
+}
